d8755bc050a3f75d33c179c9609cc5429f9030ac
Vishal Patel <vishalpatelvns3314@gmail.com>
2025-12-26 15:58:43 +0530

    Code added

---
+++ Sorting_and_Searching/1642_Sum_of_Four_Values.cpp
@@ -0,0 +1,131 @@
+
+/**
+ *    author:  vishal
+ *    created: 16.12.2025 09:54
+**/
+
+//#define NDEBUG //to use assert uncomment this
+#include <bits/stdc++.h>
+//#include "debugging.h" 
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//order_of_key (k) : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).
+using namespace std;
+//using namespace __gnu_pbds;
+using namespace chrono;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> oset;
+//typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> moset;
+#define MIN(arr) (*min_element(arr.begin(), arr.end()))
+#define MAX(arr) (*max_element(arr.begin(), arr.end()))
+#define sorts(arr) sort(arr.begin(),arr.end())
+
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+
+#define set_cnt(n) __builtin_popcountll(n)
+#define set_bit(x, i) ((x)|(1LL<<(i))) //ith bit of x to 1
+#define clear_bit(x, i) ((x)&~(1LL<<(i))) //unset
+#define toggle(x, i) ((x)^(1LL<<(i)))
+#define is_set(x, i) ((x)&(1LL<<(i)))
+#define is_pow2(x) ((x) && !((x)&((x)-1)))
+
+#define in(arr,n) for(int i=0; i<n; i++){cin>>arr[i];}
+#define out(arr,n) for(int i=0; i<n; i++){cout<<arr[i]<<" ";}
+#define matout(arr){for(int i=0; i<arr.size(); i++){for(int j=0; j<arr[0].size(); j++){cout<<arr[i][j]<<" ";}cout<<endl;}}
+#define SUM(arr) accumulate(arr.begin(), arr.end(), 0LL)
+
+#define loop(n) for(ll i=0; i<n; i++)
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define rep2(i,j,a,b,c,d) for(int i=a; i<b; i++) for(int j=c; j<d; j++)
+#define debug(x) cerr << "Line(" << __LINE__ << ") -> " << "Debug: " << #x << " = " << (x) << endl;
+#define debug2(x, y) cerr<<#x<<" = "<<x<<", "<<#y<<" = "<<y<<endl
+#define debug3(x, y, z) cerr<<#x<<" = "<<x<<", "<<#y<<" = "<<y<<", "<<#z<<" = "<<z<<endl
+#define ll long long
+const ll MOD = 1e9+7;
+#define print(arr,n) rep(i,0,n){cout<<arr[i]<<" ";}
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vb = vector<bool>;
+using vs = vector<string>;
+using vpi = vector<pii>;
+using vpll = vector<pll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vvpi = vector<vpi>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+
+//typedef __int128 int128;//not support cin cout
+//#define endl "\n"
+
+bool fun(pair<ll,ll>& p1,pair<ll,ll>& p2){
+    set<ll> st;
+    st.insert(p1.first);st.insert(p2.first);
+    st.insert(p1.second);st.insert(p2.second);
+    return (int)(st.size())==4;
+}
+void solve(){
+    ll n,x;cin>>n>>x;
+    vector<ll> arr(n); in(arr,n);
+
+    map<ll,vector<pair<ll,ll>>> mp;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            mp[arr[i]+arr[j]].push_back({i+1,j+1});
+        }
+    }
+    for(auto& y: mp){
+        ll req = x-y.first;
+        auto ptr = mp.find(req);
+        if(ptr==mp.end()) continue;
+        if(y.first==req){
+            auto& v = (ptr->second);
+            int sz = (ptr->second).size();
+            for(int i=0; i<sz; i++){
+                for(int j=i+1; j<sz; j++){
+                    if(fun(v[i],v[j])){
+                        cout<<v[i].first<<" "<<v[i].second<<" "<<v[j].first<<" "<<v[j].second<<endl;
+                        return;
+                    }
+                }
+            }
+        }else{
+            int sz = (ptr->second).size();
+            auto& v = (ptr->second);
+            auto& v2 = y.second;
+            int sz2 = v2.size();
+            for(int i=0; i<sz2; i++){
+                for(int j=0; j<sz; j++){
+                    if(fun(v2[i],v[j])){
+                        cout<<v2[i].first<<" "<<v2[i].second<<" "<<v[j].first<<" "<<v[j].second<<endl;return;
+                    }
+                }
+            }
+        }
+    }
+    cout<<"IMPOSSIBLE";
+    cout<<endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll t = 1;
+    // cin>>t;
+    auto start = high_resolution_clock::now();
+    while(t--){
+        solve();
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    //cout<<"Execution time: "<<duration.count()<<" ms"<<endl;
+
+    return 0;
+}
